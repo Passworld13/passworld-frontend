bb3bd92c0fd3480e6ca63939e931e0f128071848
zekri <marcoz@MacBook-Pro-de-zekri.local>
2025-04-09 15:57:19 +0200

    Initial commit - Frontend Vite

---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", paddingTop: "5rem" }}>
+      <img src="/logo.png" alt="Passworld logo" style={{ maxWidth: "80%", height: "auto" }} />
+    </div>
+  );
+};
+
+export default Home;
